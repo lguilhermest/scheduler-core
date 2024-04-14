3bd35810f904eeb4691e5c54f6268f2fe96cd2f2
lguilermest <lsguilhermest@gmail.com>
2024-04-13 22:13:14 -0300

    change router

--- app/routes/router.ts
+++ app/routes/router.ts
@@ -1,17 +1,60 @@
-import { Router as ExpressRouter } from "express";
+import {
+  Router as ExpressRouter,
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response
+} from "express";
+import { HttpException } from "app/exceptions";
+import { validationResult } from "express-validator";
 
-export abstract class Router {
-  protected router: ExpressRouter;
+export default class Router {
+  private router: ExpressRouter;
 
   constructor() {
     this.router = ExpressRouter();
   }
 
-  init() {
+  private asyncHandler(handlers: RequestHandler[]) {
+    return handlers.map((handler) => {
+      if (typeof handler !== 'function') {
+        return handler;
+      }
+      return function (req: Request, res: Response, next: NextFunction) {
+        const result = validationResult(req);
 
+        if (!result.isEmpty()) {
+          throw new HttpException(422, "validation error", result.array());
+        }
+
+        Promise
+          .resolve(handler(req, res, next))
+          .catch((error) => { next(error); });
+      }
+    })
   }
 
-  getRouter() {
+  public get(path: string, ...handlers: Array<RequestHandler | any>) {
+    this.router.get(path, this.asyncHandler(handlers));
+  }
+
+  public post(path: string, ...handlers: Array<RequestHandler | any>) {
+    this.router.post(path, this.asyncHandler(handlers));
+  }
+
+  public put(path: string, ...handlers: Array<RequestHandler | any>) {
+    this.router.put(path, this.asyncHandler(handlers));
+  }
+
+  public delete(path: string, ...handlers: Array<RequestHandler | any>) {
+    this.router.delete(path, this.asyncHandler(handlers));
+  }
+
+  public use(...handlers: Array<RequestHandler>) {
+    this.router.use(...handlers)
+  }
+
+  public getRouter(){
     return this.router;
   }
 }
